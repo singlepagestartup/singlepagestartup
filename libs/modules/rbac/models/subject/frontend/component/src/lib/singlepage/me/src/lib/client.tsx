f9f49712599475757f4039a97056c979196249e3
rogwild <flakedevcode@gmail.com>
2024-12-02 01:59:33 +0530

    fast me

--- libs/modules/rbac/models/subject/frontend/component/src/lib/singlepage/me/src/lib/client.tsx
+++ libs/modules/rbac/models/subject/frontend/component/src/lib/singlepage/me/src/lib/client.tsx
@@ -1,7 +1,7 @@
 "use client";
 import "client-only";
 
-import { IComponentProps } from "./interface";
+import { IComponentProps, IModel } from "./interface";
 import { api } from "@sps/rbac/models/subject/sdk/client";
 import { useEffect } from "react";
 import { useCookies } from "react-cookie";
@@ -10,48 +10,52 @@ import Cookie from "js-cookie";
 import { Skeleton } from "./Skeleton";
 
 export default function Client(props: IComponentProps) {
-  const { data, isError, refetch, isLoading } = api.me({
-    ...props.apiProps,
-    reactQueryOptions: { enabled: false },
-    mute: true,
-  });
+  // const { data, isError, refetch, isLoading } = api.me({
+  //   ...props.apiProps,
+  //   reactQueryOptions: { enabled: false },
+  //   mute: true,
+  // });
   const [jwtCookies] = useCookies(["rbac.subject.jwt"]);
 
   const token = useJwt<{
     exp: number;
     iat: number;
-    subject: { id: string };
+    subject: IModel;
   }>(jwtCookies["rbac.subject.jwt"]);
+
+  // useEffect(() => {
+  //   if (
+  //     token.decodedToken &&
+  //     !token.isExpired &&
+  //     data?.["id"] !== token.decodedToken?.["subject"]?.["id"]
+  //   ) {
+  //     refetch();
+  //   }
+  // }, [token.isExpired, token.decodedToken, data]);
+
+  // useEffect(() => {
+  //   if (isError) {
+  //     Cookie.remove("rbac.subject.jwt");
+  //     localStorage.removeItem("rbac.subject.refresh");
+  //   }
+  // }, [isError]);
 
   useEffect(() => {
     if (
-      token.decodedToken &&
-      !token.isExpired &&
-      data?.["id"] !== token.decodedToken?.["subject"]?.["id"]
+      props.set &&
+      typeof props.set === "function" &&
+      token.decodedToken?.subject
     ) {
-      refetch();
+      props.set(token.decodedToken.subject);
     }
-  }, [token.isExpired, token.decodedToken, data]);
+  }, [token.decodedToken, props]);
 
-  useEffect(() => {
-    if (isError) {
-      Cookie.remove("rbac.subject.jwt");
-      localStorage.removeItem("rbac.subject.refresh");
-    }
-  }, [isError]);
-
-  useEffect(() => {
-    if (props.set && typeof props.set === "function") {
-      props.set(data);
-    }
-  }, [data, props]);
-
-  if (isLoading) {
-    return props.skeleton || <Skeleton />;
+  if (!token.decodedToken?.subject) {
+    return props?.skeleton || <Skeleton />;
   }
 
   if (props.children) {
-    return props.children({ data });
+    return props.children({ data: token.decodedToken.subject });
   }
 
   return <></>;
