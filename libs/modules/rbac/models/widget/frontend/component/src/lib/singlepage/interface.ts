ea7c07521add2ddcf0ebddc1cbfe75a586884960
rogwild <flakedevcode@gmail.com>
2025-01-10 04:04:46 +0530

    widget

--- libs/modules/rbac/models/widget/frontend/component/src/lib/singlepage/interface.ts
+++ libs/modules/rbac/models/widget/frontend/component/src/lib/singlepage/interface.ts
@@ -1,18 +1,18 @@
-import { IComponentProps as IFindComponentProps } from "./find";
-import { IComponentProps as IAdminTableRowComponentProps } from "./admin-table-row";
-import { IComponentProps as IAdminTableComponentProps } from "./admin-table";
-import { IComponentProps as IAdminSelectInputComponentProps } from "./admin-select-input";
-import { IComponentProps as IAdminFormComponentProps } from "./admin-form";
-import { IComponentProps as IDefaultComponentProps } from "./default";
-import { IComponentProps as IIdentitiesUpdateDefaultComponentProps } from "./identities-update-default";
-import { IComponentProps as IIdentitiesCreateDefaultComponentProps } from "./identities-create-default";
-import { IComponentProps as IForgotPasswordComponentProps } from "./forgot-password";
-import { IComponentProps as ILoginComponentProps } from "./login";
-import { IComponentProps as ILogoutComponentProps } from "./logout";
-import { IComponentProps as IRegistrationComponentProps } from "./registration";
-import { IComponentProps as IResetPasswordComponentProps } from "./reset-password";
-import { IComponentProps as ISubjectOverviewDefaultComponentProps } from "./subject-overview-default";
-import { IComponentProps as ISubjectsListDefaultComponentProps } from "./subjects-list-default";
+import { IComponentProps as IFindComponentProps } from "./find/interface";
+import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row/interface";
+import { IComponentProps as IAdminTableComponentProps } from "./admin/table/interface";
+import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input/interface";
+import { IComponentProps as IAdminFormComponentProps } from "./admin/form/interface";
+import { IComponentProps as IDefaultComponentProps } from "./default/interface";
+import { IComponentProps as IIdentitiesUpdateDefaultComponentProps } from "./identities-update-default/interface";
+import { IComponentProps as IIdentitiesCreateDefaultComponentProps } from "./identities-create-default/interface";
+import { IComponentProps as IForgotPasswordComponentProps } from "./authentication/login-and-password/forgot-password-form-default/interface";
+import { IComponentProps as ILoginComponentProps } from "./authentication/select-method-default/interface";
+import { IComponentProps as ILogoutComponentProps } from "./authentication/logout-action-default/interface";
+import { IComponentProps as IRegistrationComponentProps } from "./authentication/login-and-password/registration-form-default/interface";
+import { IComponentProps as IResetPasswordComponentProps } from "./authentication/login-and-password/reset-password-form-default/interface";
+import { IComponentProps as ISubjectOverviewDefaultComponentProps } from "./subject-overview-default/interface";
+import { IComponentProps as ISubjectsListDefaultComponentProps } from "./subjects-list-default/interface";
 
 export type IComponentProps =
   | IFindComponentProps
--- libs/modules/rbac/models/widget/frontend/component/src/lib/singlepage/variants.ts
+++ libs/modules/rbac/models/widget/frontend/component/src/lib/singlepage/variants.ts
@@ -1,16 +1,16 @@
 import { Component as Find } from "./find";
-import { Component as AdminTableRow } from "./admin-table-row";
-import { Component as AdminTable } from "./admin-table";
-import { Component as AdminSelectInput } from "./admin-select-input";
-import { Component as AdminForm } from "./admin-form";
+import { Component as AdminTableRow } from "./admin/table-row";
+import { Component as AdminTable } from "./admin/table";
+import { Component as AdminSelectInput } from "./admin/select-input";
+import { Component as AdminForm } from "./admin/form";
 import { Component as Default } from "./default";
 import { Component as IdentitiesUpdateDefault } from "./identities-update-default";
 import { Component as IdentitiesCreateDefault } from "./identities-create-default";
-import { Component as ForgotPassword } from "./forgot-password";
-import { Component as Login } from "./login";
-import { Component as Logout } from "./logout";
-import { Component as Registration } from "./registration";
-import { Component as ResetPassword } from "./reset-password";
+import { Component as AuthenticationLoginAndPasswordForgotPasswordFormDefault } from "./authentication/login-and-password/forgot-password-form-default";
+import { Component as AuthenticationSelectMethodDefault } from "./authentication/select-method-default";
+import { Component as Logout } from "./authentication/logout-action-default";
+import { Component as AuthenticationLoginAndPasswordRegistrationFormDefault } from "./authentication/login-and-password/registration-form-default";
+import { Component as AuthenticationLoginAndPasswordResetPasswordFormDefault } from "./authentication/login-and-password/reset-password-form-default";
 import { Component as SubjectOverviewDefault } from "./subject-overview-default";
 import { Component as SubjectsListDefault } from "./subjects-list-default";
 
@@ -20,13 +20,16 @@ export const variants = {
   "admin-table": AdminTable,
   "admin-select-input": AdminSelectInput,
   "admin-form": AdminForm,
+  "authentication-login-and-password-forgot-password-form-default":
+    AuthenticationLoginAndPasswordForgotPasswordFormDefault,
+  "authentication-select-method-default": AuthenticationSelectMethodDefault,
+  logout: Logout,
+  "authentication-login-and-password-registration-form-default":
+    AuthenticationLoginAndPasswordRegistrationFormDefault,
+  "authentication-login-and-password-reset-password-form-default":
+    AuthenticationLoginAndPasswordResetPasswordFormDefault,
   default: Default,
   "identities-update-default": IdentitiesUpdateDefault,
-  "forgot-password": ForgotPassword,
-  login: Login,
-  logout: Logout,
-  registration: Registration,
-  "reset-password": ResetPassword,
   "subject-overview-default": SubjectOverviewDefault,
   "subjects-list-default": SubjectsListDefault,
   "identities-create-default": IdentitiesCreateDefault,
