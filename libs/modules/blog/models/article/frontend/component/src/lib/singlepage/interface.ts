62402b50e3cf3af3e37cfc5e2564214ff99e0c11
rogwild <flakedevcode@gmail.com>
2025-01-15 02:31:40 +0530

    admin

--- libs/modules/blog/models/article/frontend/component/src/lib/singlepage/interface.ts
+++ libs/modules/blog/models/article/frontend/component/src/lib/singlepage/interface.ts
@@ -1,11 +1,11 @@
-import { IComponentProps as IFindComponentProps } from "./find";
-import { IComponentProps as IAdminFormComponentProps } from "./admin/form";
-import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row";
-import { IComponentProps as IAdminTableComponentProps } from "./admin/table";
-import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input";
-import { IComponentProps as IDefaultComponentProps } from "./default";
-import { IComponentProps as IOverviewDefaultComponentProps } from "./overview-default";
-import { IComponentProps as IOverviewWithPrivateContentDefaultComponentProps } from "./overview-with-private-content-default";
+import { IComponentProps as IFindComponentProps } from "./find/interface";
+import { IComponentProps as IAdminFormComponentProps } from "./admin/form/interface";
+import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row/interface";
+import { IComponentProps as IAdminTableComponentProps } from "./admin/table/interface";
+import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input/interface";
+import { IComponentProps as IDefaultComponentProps } from "./default/interface";
+import { IComponentProps as IOverviewDefaultComponentProps } from "./overview-default/interface";
+import { IComponentProps as IOverviewWithPrivateContentDefaultComponentProps } from "./overview-with-private-content-default/interface";
 
 export type IComponentProps =
   | IFindComponentProps
--- libs/modules/blog/models/widget/frontend/component/src/lib/singlepage/interface.ts
+++ libs/modules/blog/models/widget/frontend/component/src/lib/singlepage/interface.ts
@@ -1,12 +1,12 @@
-import { IComponentProps as IFindComponentProps } from "./find";
-import { IComponentProps as IAdminFormComponentProps } from "./admin/form";
-import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row";
-import { IComponentProps as IAdminTableComponentProps } from "./admin/table";
-import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input";
-import { IComponentProps as IDefaultComponentProps } from "./default";
-import { IComponentProps as IArticlesListDefaultComponentProps } from "./articles-list-default";
-import { IComponentProps as IArticleOverviewDefaultComponentProps } from "./article-overview-default";
-import { IComponentProps as IArticleOverviewWithPrivateContentDefaultComponentProps } from "./article-overview-with-private-content-default";
+import { IComponentProps as IFindComponentProps } from "./find/interface";
+import { IComponentProps as IAdminFormComponentProps } from "./admin/form/interface";
+import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row/interface";
+import { IComponentProps as IAdminTableComponentProps } from "./admin/table/interface";
+import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input/interface";
+import { IComponentProps as IDefaultComponentProps } from "./default/interface";
+import { IComponentProps as IArticlesListDefaultComponentProps } from "./articles-list-default/interface";
+import { IComponentProps as IArticleOverviewDefaultComponentProps } from "./article-overview-default/interface";
+import { IComponentProps as IArticleOverviewWithPrivateContentDefaultComponentProps } from "./article-overview-with-private-content-default/interface";
 
 export type IComponentProps =
   | IFindComponentProps
