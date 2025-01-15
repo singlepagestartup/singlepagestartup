76a9e613011e26947d41f6eb91f83e55e221686b
rogwild <flakedevcode@gmail.com>
2025-01-15 17:02:23 +0530

    host

--- libs/modules/host/models/page/frontend/component/src/lib/singlepage/interface.ts
+++ libs/modules/host/models/page/frontend/component/src/lib/singlepage/interface.ts
@@ -1,11 +1,11 @@
-import { IComponentProps as IUrlSegmentValueComponentProps } from "./url-segment-value";
-import { IComponentProps as IFindByUrlComponentProps } from "./find-by-url";
-import { IComponentProps as IFindComponentProps } from "./find";
-import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row";
-import { IComponentProps as IAdminTableComponentProps } from "./admin/table";
-import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input";
-import { IComponentProps as IAdminFormComponentProps } from "./admin/form";
-import { IComponentProps as IDefaultComponentProps } from "./default";
+import { IComponentProps as IUrlSegmentValueComponentProps } from "./url-segment-value/interface";
+import { IComponentProps as IFindByUrlComponentProps } from "./find-by-url/interface";
+import { IComponentProps as IFindComponentProps } from "./find/interface";
+import { IComponentProps as IAdminTableRowComponentProps } from "./admin/table-row/interface";
+import { IComponentProps as IAdminTableComponentProps } from "./admin/table/interface";
+import { IComponentProps as IAdminSelectInputComponentProps } from "./admin/select-input/interface";
+import { IComponentProps as IAdminFormComponentProps } from "./admin/form/interface";
+import { IComponentProps as IDefaultComponentProps } from "./default/interface";
 export type IComponentProps =
   | IUrlSegmentValueComponentProps
   | IFindByUrlComponentProps
