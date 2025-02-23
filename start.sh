3fa893ed061fe9185a7f0e2480ce278ff7de9029
rogwild <flakedevcode@gmail.com>
2025-02-23 22:46:28 +0300

    start

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Run host app
-# if first passed argument is "host" run npm run host:start
 if [ "$1" = "host" ]; then
   ./create_env.sh host deployment
   npm run host:start
   exit 0
-else if [ "$1" = "api" ]; then
+elif [ "$1" = "api" ]; then
   ./create_env.sh api deployment
   ./migrate.sh seed &
   npm run api:start
   exit 0
 fi
+
+echo "Usage: ./start.sh [host|api]"
+exit 1
