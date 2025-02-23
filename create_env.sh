698cda9165525c24ac709654591798f9c8f3d429
rogwild <flakedevcode@gmail.com>
2025-02-24 00:20:51 +0300

    create env

--- create_env.sh
+++ create_env.sh
@@ -2,16 +2,14 @@
 
 if [ "$1" = "host" ]; then
   if [ "$2" = "deployment" ]; then
-    production_env=$(cat apps/host/.env.production)
-    echo $production_env > apps/host/.env.local
-    echo $(printenv) > apps/host/.env.local
+    cat apps/host/.env.production > apps/host/.env.local
+    printenv >> apps/host/.env.local
     exit 0
   fi
-else if [ "$1" = "api" ]; then
+elif [ "$1" = "api" ]; then
   if [ "$2" = "deployment" ]; then
-    production_env=$(cat apps/api/.env.production)
-    echo $production_env > apps/api/.env
-    echo $(printenv) > apps/api/.env
+    cat apps/api/.env.production > apps/api/.env
+    printenv >> apps/api/.env
     exit 0
   fi
 fi
