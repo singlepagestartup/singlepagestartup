ced37b6afa30f00a95b6162b25953bb6bb754ee5
rogwild <flakedevcode@gmail.com>
2025-03-23 10:30:48 +0300

    NEXT_PUBLIC_HOST_SERVICE_URL

--- libs/shared/utils/src/lib/envs/host.ts
+++ libs/shared/utils/src/lib/envs/host.ts
@@ -11,6 +11,8 @@ export const HOST_SERVICE_URL =
   process.env["NEXT_PUBLIC_HOST_SERVICE_URL"] ||
   process.env["HOST_SERVICE_URL"] ||
   "http://localhost:3000";
+export const NEXT_PUBLIC_HOST_SERVICE_URL =
+  process.env["NEXT_PUBLIC_HOST_SERVICE_URL"] || "http://localhost:3000";
 export const STALE_TIME =
   Number(process.env["NEXT_PUBLIC_STALE_TIME"]) || 60 * 1000;
 export const REVALIDATE: number | undefined =
