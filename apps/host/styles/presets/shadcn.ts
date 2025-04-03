c83ca59500a55903ebb4e27326cbf8cd141c94eb
rogwild <flakedevcode@gmail.com>
2025-04-03 16:34:42 +0300

    cols

--- apps/host/styles/presets/shadcn.ts
+++ apps/host/styles/presets/shadcn.ts
@@ -182,6 +182,10 @@ export const preset = {
     {
       pattern: /object-(cover|contain)/,
     },
+    {
+      pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+      variants: ["sm", "md", "lg", "xl", "2xl"],
+    },
   ],
   plugins: [typographyPlugin, aspectRatioPlugin, animatePlugin, shadcnPlugin],
 } satisfies Config;
