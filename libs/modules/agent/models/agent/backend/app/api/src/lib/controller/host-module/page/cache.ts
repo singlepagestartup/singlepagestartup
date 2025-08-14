bb28c5fcbf241310510039846f4e27b7f69649de
rogwild <flakedevcode@gmail.com>
2025-08-14 23:56:21 +0300

    cache without notification

--- libs/modules/agent/models/agent/backend/app/api/src/lib/controller/host-module/page/cache.ts
+++ libs/modules/agent/models/agent/backend/app/api/src/lib/controller/host-module/page/cache.ts
@@ -2,11 +2,6 @@ import { HOST_SERVICE_URL, RBAC_SECRET_KEY } from "@sps/shared-utils";
 import { Context } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { Service } from "../../../service";
-import { api as notificationTopicApi } from "@sps/notification/models/topic/sdk/server";
-import { api as notificationTemplateApi } from "@sps/notification/models/template/sdk/server";
-import { api as notificationNotificationApi } from "@sps/notification/models/notification/sdk/server";
-import { api as notificationNotificationsToTemplatesApi } from "@sps/notification/relations/notifications-to-templates/sdk/server";
-import { api as notificationTopicsToNotificationsApi } from "@sps/notification/relations/topics-to-notifications/sdk/server";
 import { logger } from "@sps/backend-utils";
 import { api as hostModulePageApi } from "@sps/host/models/page/sdk/server";
 
@@ -24,17 +19,6 @@ export class Handler {
       }
 
       logger.info("Host module page cache started");
-
-      const topics = await notificationTopicApi.find({
-        params: {
-          filters: {
-            and: [{ column: "slug", method: "eq", value: "information" }],
-          },
-        },
-        options: {
-          headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-        },
-      });
 
       const urls = await hostModulePageApi.urls({});
 
@@ -65,75 +49,6 @@ export class Handler {
               page: path,
               error: err,
             });
-          }
-        }
-      }
-
-      if (topics?.length) {
-        const topic = topics[0];
-
-        const templates = await notificationTemplateApi.find({
-          params: {
-            filters: {
-              and: [{ column: "variant", method: "eq", value: "agent-result" }],
-            },
-          },
-          options: {
-            headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-          },
-        });
-
-        if (templates?.length) {
-          for (const template of templates) {
-            const notifications = [
-              {
-                data: JSON.stringify({ title: "Agent finished" }),
-                method: "email",
-                reciever: "rogwild.design@gmail.com",
-                attachments: "[]",
-              },
-            ];
-
-            for (const notification of notifications) {
-              const createdNotification =
-                await notificationNotificationApi.create({
-                  data: { ...notification },
-                  options: {
-                    headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-                  },
-                });
-
-              if (!createdNotification) {
-                throw new Error("Failed to create notification");
-              }
-
-              await notificationNotificationsToTemplatesApi.create({
-                data: {
-                  notificationId: createdNotification.id,
-                  templateId: template.id,
-                },
-                options: {
-                  headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-                },
-              });
-
-              await notificationTopicsToNotificationsApi.create({
-                data: {
-                  topicId: topic.id,
-                  notificationId: createdNotification.id,
-                },
-                options: {
-                  headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-                },
-              });
-
-              await notificationNotificationApi.send({
-                id: createdNotification.id,
-                options: {
-                  headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-                },
-              });
-            }
           }
         }
       }
