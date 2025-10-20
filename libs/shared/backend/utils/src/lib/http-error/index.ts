016ede44d4b4a83262138c12ae8dda5f8fc100ae
FiaLDI <leon.youtube2017@yandex.ru>
2025-10-20 15:47:02 +0300

    feat: with regexp only

--- libs/shared/backend/utils/src/lib/http-error/index.ts
+++ libs/shared/backend/utils/src/lib/http-error/index.ts
@@ -1,6 +1,7 @@
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { httpErrorPatterns } from "./paterns";
 
+// Вспомогательные функции:
 function extractMessage(error: any): string {
   if (!error) return "";
   if (typeof error === "string") return error;
@@ -8,7 +9,6 @@ function extractMessage(error: any): string {
   if (error.cause) return extractMessage(error.cause);
   return String(error);
 }
-
 function extractOriginalError(error: any): any {
   if (error?.cause) return extractOriginalError(error.cause);
   return error;
@@ -17,21 +17,16 @@ function extractOriginalError(error: any): any {
 export function util(error: any): {
   status: ContentfulStatusCode;
   message: string;
-  details?: any; // исходная ошибка как есть
+  details?: any;
 } {
   const message = extractMessage(error) || "Unknown error";
   const details = extractOriginalError(error);
 
   for (const [status, patterns] of httpErrorPatterns) {
-    if (
-      patterns.some((p) =>
-        typeof p === "string" ? message.includes(p) : p.test(message),
-      )
-    ) {
+    if (patterns.some((regex) => regex.test(message))) {
       return { status, message, details };
     }
   }
-
   return {
     status: 500,
     message: `Internal server error: ${message}`,
