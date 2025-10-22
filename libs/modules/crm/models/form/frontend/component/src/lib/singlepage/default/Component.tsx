74cf01c1294ef129838ca77c035a5d0cace50697
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-10-22 08:37:18 +0300

    fix: crm form model html

--- libs/modules/crm/models/form/frontend/component/src/lib/singlepage/default/Component.tsx
+++ libs/modules/crm/models/form/frontend/component/src/lib/singlepage/default/Component.tsx
@@ -1,5 +1,20 @@
+import { cn } from "@sps/shared-frontend-client-utils";
 import { IComponentPropsExtended } from "./interface";
 
 export function Component(props: IComponentPropsExtended) {
-  return props.children;
+  return (
+    <div
+      data-module="crm"
+      data-model="form"
+      data-id={props.data?.id || ""}
+      data-variant={props.variant}
+      className={cn(
+        "w-full flex flex-col",
+        props.data.className,
+        props.className,
+      )}
+    >
+      {props.children}
+    </div>
+  );
 }
