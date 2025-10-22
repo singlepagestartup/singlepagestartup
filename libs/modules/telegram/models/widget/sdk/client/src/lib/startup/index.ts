cbbf1d428393bfd14fe27af346a3215ecc378d68
RockyAtoyan <111198613+RockyAtoyan@users.noreply.github.com>
2025-10-22 08:45:14 +0300

    fix: telegram module client sdk export

--- libs/modules/telegram/models/widget/sdk/client/src/lib/startup/index.ts
+++ libs/modules/telegram/models/widget/sdk/client/src/lib/startup/index.ts
@@ -1,6 +1,14 @@
 "use client";
 
-import { api as parentApi } from "../singlepage";
+import {
+  api as parentApi,
+  type IProps as IParentProps,
+  type IResult as IParentResult,
+} from "../singlepage";
 export { Provider, queryClient } from "../singlepage";
+
+export type IProps = IParentProps & {};
+
+export type IResult = IParentResult & {};
 
 export const api = parentApi;
