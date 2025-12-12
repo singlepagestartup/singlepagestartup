e46a766cfc51cffbe14f911fcdcd562324fc616f
rogwild <flakedevcode@gmail.com>
2025-12-13 04:19:57 +0500

    dummy

--- libs/modules/agent/models/agent/backend/app/api/src/lib/controller/singlepage/dummy.ts
+++ libs/modules/agent/models/agent/backend/app/api/src/lib/controller/singlepage/dummy.ts
@@ -2,11 +2,6 @@ import { RBAC_SECRET_KEY } from "@sps/shared-utils";
 import { Context } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { Service } from "../../service";
-import { api as notificationTopicApi } from "@sps/notification/models/topic/sdk/server";
-import { api as notificationTemplateApi } from "@sps/notification/models/template/sdk/server";
-import { api as notificationNotificationApi } from "@sps/notification/models/notification/sdk/server";
-import { api as notificationNotificationsToTemplatesApi } from "@sps/notification/relations/notifications-to-templates/sdk/server";
-import { api as notificationTopicsToNotificationsApi } from "@sps/notification/relations/topics-to-notifications/sdk/server";
 import { getHttpErrorType, logger } from "@sps/backend-utils";
 export class Handler {
   service: Service;
@@ -22,89 +17,6 @@ export class Handler {
       }
 
       logger.info("Dummy started");
-
-      const topics = await notificationTopicApi.find({
-        params: {
-          filters: {
-            and: [{ column: "slug", method: "eq", value: "information" }],
-          },
-        },
-        options: {
-          headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-        },
-      });
-
-      if (!topics?.length) {
-        throw new Error("Not Found error. No topic found");
-      }
-
-      const topic = topics[0];
-
-      const templates = await notificationTemplateApi.find({
-        params: {
-          filters: {
-            and: [{ column: "variant", method: "eq", value: "agent-result" }],
-          },
-        },
-        options: {
-          headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-        },
-      });
-
-      if (!templates?.length) {
-        throw new Error("Not Found error. No template found");
-      }
-      const template = templates[0];
-
-      const notifications = [
-        {
-          data: JSON.stringify({ title: "Agent finished" }),
-          method: "email",
-          reciever: "rogwild.design@gmail.com",
-          attachments: "[]",
-        },
-      ];
-
-      for (const notification of notifications) {
-        const createdNotification = await notificationNotificationApi.create({
-          data: { ...notification },
-          options: {
-            headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-          },
-        });
-
-        if (!createdNotification) {
-          throw new Error("Internal error. Failed to create notification");
-        }
-
-        await notificationNotificationsToTemplatesApi.create({
-          data: {
-            notificationId: createdNotification.id,
-            templateId: template.id,
-          },
-          options: {
-            headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-          },
-        });
-
-        await notificationTopicsToNotificationsApi.create({
-          data: {
-            topicId: topic.id,
-            notificationId: createdNotification.id,
-          },
-          options: {
-            headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-          },
-        });
-
-        await notificationNotificationApi.send({
-          id: createdNotification.id,
-          options: {
-            headers: { "X-RBAC-SECRET-KEY": RBAC_SECRET_KEY },
-          },
-        });
-      }
-
       logger.info("Dummy finished");
 
       return c.json({ data: { ok: true } });
