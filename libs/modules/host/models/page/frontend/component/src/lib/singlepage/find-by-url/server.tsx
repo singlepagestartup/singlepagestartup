79a4f040206e08aa8378e2d190857af24be5920a
rogwild <flakedevcode@gmail.com>
2025-12-25 00:09:29 +0500

    catch

--- libs/modules/host/models/page/frontend/component/src/lib/singlepage/find-by-url/server.tsx
+++ libs/modules/host/models/page/frontend/component/src/lib/singlepage/find-by-url/server.tsx
@@ -5,14 +5,18 @@ import { IComponentProps } from "./interface";
 import { api } from "@sps/host/models/page/sdk/server";
 
 export default async function Server(props: IComponentProps) {
-  const data = await api.findByUrl({
-    url: props.url,
-    catchErrors: true,
-  });
+  const data = await api
+    .findByUrl({
+      url: props.url,
+      catchErrors: true,
+    })
+    .catch((error) => {
+      //
+    });
 
   if (!props.children || typeof props.children !== "function") {
     return;
   }
 
-  return props.children({ data });
+  return props.children({ data: data || undefined });
 }
