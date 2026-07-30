a5cde69765ff8bece2140b6c5c92d16960f9fa6f
rogwild <flakedevcode@gmail.com>
2026-07-30 15:20:55 +0300

    Support Google verification files in host middleware

--- apps/host/middleware.ts
+++ apps/host/middleware.ts
@@ -23,6 +23,6 @@ export async function middleware(request: any) {
 
 export const config = {
   matcher: [
-    "/((?!_next|images|_next/static|_next/image|sitemap|robots|api|favicon|healthz).*)",
+    "/((?!_next|images|_next/static|_next/image|sitemap|robots|api|favicon|healthz|google[a-zA-Z0-9]+\\.html).*)",
   ],
 };
